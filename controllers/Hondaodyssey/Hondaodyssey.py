c96e6c24bfa36df17e150ced2453ab076d0b18f8
August <august.horii@gmail.com>
2026-03-31 12:38:10 +0900

    Some bit of changes
    I've calibrated the target x and y of lanes and figured out how to stabilize the car on a two dashed and a one dashed lane. Now I need to figure out the transition from a twodashed to a one dashed

--- controllers/Hondaodyssey/Hondaodyssey.py
+++ controllers/Hondaodyssey/Hondaodyssey.py
@@ -58,6 +58,11 @@ last_steering = 0.0
 integral = 0.0
 last_average_x = 0.0
 was_blind = False 
+last_visible_cameras = 0
+#Adding a new variable: angle filter buffer
+#angle_filter_buffer = [0.0, 0.0, 0.0]
+angle_filter_buffer = [0.0, 0.0, 0.0]
+
 print("Use the up/down/left/right buttons to move")
 print("Press A to start the AUTOPILOT")
 
@@ -141,7 +146,7 @@ while driver.step() != -1:
                         pixel_count_right += 1  
             if pixel_count_right > 0:
                 avg_x_r = sum_x_r / pixel_count_right
-                target_x_r = width * 0.7
+                target_x_r = width * 0.75
                 error += (avg_x_r - target_x_r) / width
                 
                 if avg_x_r > width * 0.85:
@@ -164,7 +169,7 @@ while driver.step() != -1:
                         pixel_count_left += 1
             if pixel_count_left > 0:
                 avg_x_l = sum_x_l / pixel_count_left
-                target_x_l = width * 0.2
+                target_x_l = width * 0.15
                 error += (avg_x_l - target_x_l) / width
                 visible_cameras += 1    
         #DUAL PID
@@ -173,9 +178,14 @@ while driver.step() != -1:
             #Apply the pid formula to error to get the steering angle just like before
             #Possibly reduce the speed on each corner depending on later tests
             #save the errors for memory
-        if visible_cameras > 0:
-            error = error / visible_cameras
-            if was_blind:
+        if visible_cameras == 1:
+            error = error * 0.5
+            #look only 3 frame-angles to smoothe out movement of the car
+            angle_filter_buffer.pop(0)
+            angle_filter_buffer.append(error)
+            error = sum(angle_filter_buffer) / 3.0
+            
+            if was_blind or visible_cameras != last_visible_cameras:
                 last_error = error
                 was_blind = False
             if (error > 0) != (last_error > 0):
@@ -188,6 +198,12 @@ while driver.step() != -1:
             d_term = (error - last_error) * 5.0
             
             current_steering = p_term + i_term + d_term
+            
+            if current_steering - last_steering > 0.1:
+                current_steering = last_steering + 0.1
+            elif current_steering - last_steering < -0.1:
+                current_steering = last_steering - 0.1
+                
             turn_factor = 1.0 - abs(current_steering) * 1.2
             current_speed = max(15.0, 20.0 * turn_factor)
             last_error = error
@@ -205,14 +221,19 @@ while driver.step() != -1:
         else:
             
             was_blind = True
+            last_visible_cameras = 0
             driver.setBrakeIntensity(0.0)
-            target_x_r = width * 0.7
-            displacement = (last_average_x - target_x_r) / width
-            recovery_steering = last_steering + (displacement * 1.5)
-            recovery_steering = max(-0.5, min(0.5, recovery_steering))
-            current_speed = max(5.0, 15.0 - abs(displacement) * 30.0)
+            current_steering = last_steering
+            current_speed = 10.0
             
-            current_steering = recovery_steering
+            
+            #target_x_r = width * 0.75
+            #displacement = (last_average_x - target_x_r) / width
+            #recovery_steering = last_steering + (displacement * 1.5)
+            #recovery_steering = max(-0.5, min(0.5, recovery_steering))
+            #current_speed = max(5.0, 15.0 - abs(displacement) * 30.0)
+            
+            #current_steering = recovery_steering
             
              
             print(f"Cameras active: {visible_cameras} ||| L_Line is at: {pos_l:.2f} ||| R_Line is at: {pos_r:.2f} ||| Steer: {current_steering:.2f} ||| Speed: {current_speed:.1f} ||| Error: {error:.3f} ||| P:{p_term:.2f} I:{i_term:.2f} D:{d_term:.2f}")
